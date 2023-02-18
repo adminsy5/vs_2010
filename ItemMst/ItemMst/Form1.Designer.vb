e567261796fe3f226df649a80bd0b7f2ab078888
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-18 08:46:19 +0530

    itemMst Added

---
+++ ItemMst/ItemMst/Form1.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridViewReader = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSearch = New System.Windows.Forms.Button()
+        Me.TextBoxSrch = New System.Windows.Forms.TextBox()
+        Me.TextBoxCost = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxQuantity = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxItemNo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonRefresh = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStripData = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        CType(Me.DataGridViewReader, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.ContextMenuStripData.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridViewReader
+        '
+        Me.DataGridViewReader.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridViewReader.ContextMenuStrip = Me.ContextMenuStripData
+        Me.DataGridViewReader.Location = New System.Drawing.Point(863, 35)
+        Me.DataGridViewReader.Name = "DataGridViewReader"
+        Me.DataGridViewReader.RowTemplate.Height = 33
+        Me.DataGridViewReader.Size = New System.Drawing.Size(886, 803)
+        Me.DataGridViewReader.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ButtonSearch)
+        Me.GroupBox1.Controls.Add(Me.TextBoxSrch)
+        Me.GroupBox1.Controls.Add(Me.TextBoxCost)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBoxQuantity)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBoxName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBoxItemNo)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ButtonSave)
+        Me.GroupBox1.Controls.Add(Me.ButtonUpdate)
+        Me.GroupBox1.Controls.Add(Me.ButtonDelete)
+        Me.GroupBox1.Controls.Add(Me.ButtonRefresh)
+        Me.GroupBox1.Controls.Add(Me.ButtonClear)
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(798, 803)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Item Mst"
+        '
+        'ButtonSearch
+        '
+        Me.ButtonSearch.Location = New System.Drawing.Point(231, 386)
+        Me.ButtonSearch.Name = "ButtonSearch"
+        Me.ButtonSearch.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonSearch.TabIndex = 14
+        Me.ButtonSearch.Text = "Search"
+        Me.ButtonSearch.UseVisualStyleBackColor = True
+        '
+        'TextBoxSrch
+        '
+        Me.TextBoxSrch.Location = New System.Drawing.Point(411, 386)
+        Me.TextBoxSrch.Multiline = True
+        Me.TextBoxSrch.Name = "TextBoxSrch"
+        Me.TextBoxSrch.Size = New System.Drawing.Size(174, 52)
+        Me.TextBoxSrch.TabIndex = 13
+        '
+        'TextBoxCost
+        '
+        Me.TextBoxCost.Location = New System.Drawing.Point(542, 161)
+        Me.TextBoxCost.Name = "TextBoxCost"
+        Me.TextBoxCost.Size = New System.Drawing.Size(206, 31)
+        Me.TextBoxCost.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(417, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 25)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Cost"
+        '
+        'TextBoxQuantity
+        '
+        Me.TextBoxQuantity.Location = New System.Drawing.Point(542, 234)
+        Me.TextBoxQuantity.Name = "TextBoxQuantity"
+        Me.TextBoxQuantity.Size = New System.Drawing.Size(206, 31)
+        Me.TextBoxQuantity.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(417, 234)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 25)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Quantity"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(186, 234)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(198, 31)
+        Me.TextBoxName.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(61, 234)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 25)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Name"
+        '
+        'TextBoxItemNo
+        '
+        Me.TextBoxItemNo.Location = New System.Drawing.Point(186, 161)
+        Me.TextBoxItemNo.Name = "TextBoxItemNo"
+        Me.TextBoxItemNo.Size = New System.Drawing.Size(198, 31)
+        Me.TextBoxItemNo.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 161)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Item number"
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(44, 534)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonSave.TabIndex = 4
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(617, 534)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonUpdate.TabIndex = 3
+        Me.ButtonUpdate.Text = "Update"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(617, 650)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonDelete.TabIndex = 2
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonRefresh
+        '
+        Me.ButtonRefresh.Location = New System.Drawing.Point(312, 650)
+        Me.ButtonRefresh.Name = "ButtonRefresh"
+        Me.ButtonRefresh.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonRefresh.TabIndex = 1
+        Me.ButtonRefresh.Text = "Refresh"
+        Me.ButtonRefresh.UseVisualStyleBackColor = True
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(44, 650)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(153, 52)
+        Me.ButtonClear.TabIndex = 0
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(160, 36)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(160, 36)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'ContextMenuStripData
+        '
+        Me.ContextMenuStripData.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.ContextMenuStripData.Name = "ContextMenuStripData"
+        Me.ContextMenuStripData.Size = New System.Drawing.Size(161, 76)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1898, 923)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridViewReader)
+        Me.Name = "Form1"
+        Me.Text = "Item Information"
+        CType(Me.DataGridViewReader, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ContextMenuStripData.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridViewReader As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBoxItemNo As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents ButtonUpdate As System.Windows.Forms.Button
+    Friend WithEvents ButtonDelete As System.Windows.Forms.Button
+    Friend WithEvents ButtonRefresh As System.Windows.Forms.Button
+    Friend WithEvents ButtonClear As System.Windows.Forms.Button
+    Friend WithEvents TextBoxCost As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxQuantity As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ButtonSearch As System.Windows.Forms.Button
+    Friend WithEvents TextBoxSrch As System.Windows.Forms.TextBox
+    Friend WithEvents ContextMenuStripData As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ ItemMst/ItemMst/Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim con As SqlConnection
+    Dim cmd As SqlCommand
+    Dim ad As SqlDataAdapter
+    Dim dt As DataSet
+
+    Private Sub ButtonSave_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSave.Click
+        Connection()
+        createTable()
+        insertData()
+        ButtonClear_Click(sender, e)
+        showdata()
+    End Sub
+
+    Private Sub Connection()
+        con = New SqlConnection("Data Source=.\SQLEXPRESS;AttachDbFilename=B:\404-Vb.Net\vs_2010\ItemMst\ItemMst\Database1.mdf;Integrated Security=True;User Instance=True")
+        Try
+            con.Open()
+            If con.State = ConnectionState.Open Then
+                'MsgBox("Connected")
+            End If
+        Catch ec As Exception
+            'MsgBox(ec.Message)
+        End Try
+    End Sub
+
+    Private Sub createTable()
+        cmd = New SqlCommand("create table itemMst(itemno int primary key,name varchar(20),cost int,quantity int)", Connection:=con)
+        Try
+            cmd.ExecuteNonQuery()
+            'MsgBox("Create Table ")
+        Catch ex As Exception
+            'MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub insertData()
+        Dim itemno As Integer = Convert.ToInt32(TextBoxItemNo.Text)
+        Dim name As String = TextBoxName.Text
+        Dim cost As Integer = Convert.ToInt32(TextBoxCost.Text)
+        Dim quantity As Integer = Convert.ToInt32(TextBoxQuantity.Text)
+
+        cmd = New SqlCommand("insert into itemMst values(@itemno,@name,@cost,@quantity)", Connection:=con)
+        cmd.Parameters.AddWithValue("@itemno", itemno)
+        cmd.Parameters.AddWithValue("@name", name)
+        cmd.Parameters.AddWithValue("@cost", cost)
+        cmd.Parameters.AddWithValue("@quantity", quantity)
+        Try
+            cmd.ExecuteNonQuery()
+            MsgBox("insert!")
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+    End Sub
+
+    Private Sub showdata()
+        ad = New SqlDataAdapter("select * from itemMst", selectConnection:=con)
+        dt = New DataSet
+        ad.Fill(dt, "itemMst")
+        DataGridViewReader.DataSource = dt.Tables(0).DefaultView
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As System.Object, e As System.EventArgs) Handles ButtonDelete.Click
+        Connection()
+        If DataGridViewReader.RowCount = 0 Then
+            MsgBox("Unable to deleted !")
+        End If
+
+        If MsgBox("Deleted Record ?", MsgBoxStyle.Question + MsgBoxStyle.OkCancel, "Confirmation") = MsgBoxResult.Cancel Then Return
+
+        Try
+            If DataGridViewReader.AreAllCellsSelected(0) = True Then
+                cmd = New SqlCommand("delete from itemMst", Connection:=con)
+                cmd.ExecuteNonQuery()
+                ButtonRefresh_Click(sender, e)
+                MsgBox("Deleted All")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Try
+            For Each row As DataGridViewRow In DataGridViewReader.SelectedRows
+                If row.Selected Then
+                    cmd = New SqlCommand("delete from itemMst where itemno = '" & row.DataBoundItem(0).ToString() & "'", Connection:=con)
+                    cmd.ExecuteNonQuery()
+                    ButtonRefresh_Click(sender, e)
+                    MsgBox("Deleted")
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub ButtonClear_Click(sender As System.Object, e As System.EventArgs) Handles ButtonClear.Click
+        TextBoxCost.Clear()
+        TextBoxItemNo.Clear()
+        TextBoxName.Clear()
+        TextBoxQuantity.Clear()
+        TextBoxSrch.Clear()
+    End Sub
+
+    Private Sub ButtonRefresh_Click(sender As System.Object, e As System.EventArgs) Handles ButtonRefresh.Click
+        Connection()
+        ad = New SqlDataAdapter("select * from itemMst", selectConnection:=con)
+        dt = New DataSet
+        ad.Fill(dt, "itemMst")
+        DataGridViewReader.DataSource = dt.Tables(0).DefaultView
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As System.Object, e As System.EventArgs) Handles ButtonUpdate.Click
+        Connection()
+        Dim itemno As Integer = Convert.ToInt32(TextBoxItemNo.Text)
+        Dim name As String = TextBoxName.Text
+        Dim cost As Integer = Convert.ToInt32(TextBoxCost.Text)
+        Dim quantity As Integer = Convert.ToInt32(TextBoxQuantity.Text)
+
+        cmd = New SqlCommand("Update itemMst set name=@name,cost=@cost,quantity=@quantity where itemno=@itemno", Connection:=con)
+        cmd.Parameters.AddWithValue("@itemno", itemno)
+        cmd.Parameters.AddWithValue("@name", name)
+        cmd.Parameters.AddWithValue("@cost", cost)
+        cmd.Parameters.AddWithValue("@quantity", quantity)
+        Try
+            cmd.ExecuteNonQuery()
+            ButtonRefresh_Click(sender, e)
+            MsgBox("Updated")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonSearch_Click(sender As System.Object, e As System.EventArgs) Handles ButtonSearch.Click
+        Connection()
+        Dim name As String = TextBoxSrch.Text
+
+        ad = New SqlDataAdapter("select * from itemMst where name LIKE '" & TextBoxSrch.Text & "'", selectConnection:=con)
+        dt = New DataSet
+        ad.Fill(dt, "itemMst")
+        Try
+            DataGridViewReader.DataSource = dt.Tables(0).DefaultView
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles DeleteToolStripMenuItem.Click
+        ButtonDelete_Click(sender, e)
+    End Sub
+
+    Private Sub EditToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles EditToolStripMenuItem.Click
+        Connection()
+        cmd = New SqlCommand("Update itemMst set cost='" & TextBoxCost.Text & "',quantity='" & TextBoxQuantity.Text & "' where name LIKE '" & TextBoxSrch.Text & "'", Connection:=con)
+        Try
+            cmd.ExecuteNonQuery()
+            ButtonRefresh_Click(sender, e)
+            MsgBox("New data updated")
+        Catch ec As Exception
+            MsgBox(ec.Message)
+        End Try
+    End Sub
+End Class
